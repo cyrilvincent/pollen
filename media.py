3d7ce3966a1ce9106d1a3d902f2dd9ab2060640f
cyrilvincent <contact@cyrilvincent.com>
2019-12-06 14:19:27 +0100

    Pollen 1.75/4

--- media.py
+++ media.py
@@ -9,8 +9,9 @@ class Publisher:
         self.name = name
 
 import datetime
+import abc
 from typing import List
-class Media:
+class Media(metaclass=abc.ABCMeta):
 
     def __init__(self, id, title, price, date = datetime.datetime.now(), publisher : Publisher = None, authors:List[Author] = []):
         self.id = id
@@ -21,8 +22,8 @@ class Media:
         self.authors = authors
 
     @property
-    def netPrice(self):
-        return self.price * 1.2
+    @abc.abstractmethod
+    def netPrice(self): ...
 
     def __repr__(self):
         return f"{type(self).__name__}: {self.id} {self.title}"
@@ -33,9 +34,9 @@ class Book(Media):
         super().__init__(id,title,price,date,publisher,authors)
         self.nbPage = nbPage
 
-    @property
-    def netPrice(self):
-        return self.price * 1.05 * 0.95 + 0.01
+    # @property
+    # def netPrice(self):
+    #     return self.price * 1.05 * 0.95 + 0.01
 
 
 class Cd(Media):
@@ -48,7 +49,25 @@ class Cd(Media):
     def netPrice(self):
         return self.price * 1.2 * 0.9
 
+import functools
+class Cart:
 
+    def __init__(self):
+        self.medias = []
+
+    def add(self, media:Media):
+        self.medias.append(media)
+
+    def remove(self, media:Media):
+        self.medias.remove(media)
+
+    @property
+    def totalNetPrice(self):
+        return sum([m.netPrice for m in self.medias])
+
+    @property
+    def totalNetPrice2(self):
+        return functools.reduce(lambda acc, cur : acc + cur.netPrice, self.medias, 0)
 
 
 
