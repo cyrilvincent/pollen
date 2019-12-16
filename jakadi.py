542e2c67777aa39a6a33cd1c8606dfa4f7fea4ab
cyrilvincent <contact@cyrilvincent.com>
2019-12-16 11:46:17 +0100

    Pollen 2.25/4

--- jakadi.py
+++ jakadi.py
@@ -2,11 +2,11 @@ import ctypes
 print(ctypes.windll.kernel32)
 libc = ctypes.cdll.msvcrt
 print(libc.time(None))
-lib = ctypes.CDLL("jakadi.dll") # windll est spécialisé pour les DLL et OleDb Windows
+lib = ctypes.CDLL(r"D:\CVC\ATP\pollen\jakadi.dll") # windll est spécialisé pour les DLL et OleDb Windows
 print(lib)
 res = lib.multiplier(2,3)
 print(res)
-lib.dit_papa("papa")
-lib.dit_papa("papa".encode('utf8'))
-"""s = ctypes.c_char_p('kiwi'.encode('utf8'))
-lib.jakadi(ctypes.byref(s))"""
+#lib.dit_papa("papa")
+lib.dit_papa("pépé".encode('utf8'))
+s = ctypes.c_char_p('kiwi'.encode('utf8'))
+lib.jakadi(ctypes.byref(s))
